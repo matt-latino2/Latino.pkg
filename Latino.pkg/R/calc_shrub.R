ef3bcef0073d7018a2d729e538fd38db56656d0d
Matthew Latino <matthew.latino-2@selu.edu>
2023-09-26 19:14:34 +0000

    commit back to revert

--- Latino.pkg/R/calc_shrub.R
+++ Latino.pkg/R/calc_shrub.R
@@ -2,5 +2,5 @@ est_shrub_density <- function(height, length, width){
   volume <- calc_shrub_vol(height, length, width)
   mass <- est_shrub_mass(volume)
   density <- mass/volume
-  return(densty)
+  return(density)
 }
